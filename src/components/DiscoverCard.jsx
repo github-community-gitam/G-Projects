62de1186314d6f2f85902a02460ae5fec80135d4
Premsai Varma <premsaivarmacoding@gmail.com>
2023-10-25 22:25:38 +0530

    updated the cards

--- src/components/DiscoverCard.jsx
+++ src/components/DiscoverCard.jsx
@@ -1,19 +1,21 @@
-import left_1 from '../images/left_1.png'
+// DiscoverCard.js
+import React from 'react';
 
-const DiscoverCard = () => {
-    const role = "Frotend Developer"
-    const batch = "2020 - 2024"
+const DiscoverCard = (props) => {
+    const { details } = props;
+    const { profile, name, role, batch } = details;
+
     return (
-        <div className="container max-w-xs max-h-sm m-auto my-5 border ring-1 rounded-xl">
-            <img src={left_1} className="h-3/5 w-full" />
-            <div className='h-full px-6 text-left'>
-                <h1 className='font-bold text-2xl'>Jon Snow</h1>
+        <div className="container max-w-xs m-auto my-5 border ring-1 rounded-xl">
+            <img src={profile} className="h-4/7 w-full" alt={name} />
+            <div className='h-2/7 px-6 text-left'>
+                <h1 className='font-bold text-2xl'>{name}</h1>
                 <p>{role}</p>
                 <p>{batch}</p>
             </div>
-            <button className='w-full py-2 mt-4 text-white bg-violet-700'>View full Profile</button>
+            <button className='h-1/7 relative py-2 bottom-0 w-full mt-4 text-white bg-violet-700 rounded-b-xl'>View Full Profile</button>
         </div>
-    )
+    );
 }
 
-export default DiscoverCard
+export default DiscoverCard;
